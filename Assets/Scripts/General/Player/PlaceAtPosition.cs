b55cd95279eea09291f930fce51043dbd21a0b91
IronGriddle <74552247+IronGriddle@users.noreply.github.com>
2021-04-08 21:07:26 -0600

    Changed Some stuff
    Please, I don't even remember.

--- Assets/Scripts/General/Player/PlaceAtPosition.cs
+++ Assets/Scripts/General/Player/PlaceAtPosition.cs
@@ -23,24 +23,30 @@ public class PlaceAtPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(key)){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        if (Input.GetKey(key) && !placeIsRunning){
+            StartCoroutine(Place());
+        }
+    }
 
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                Debug.DrawLine(ray.origin, hit.point);
+    float placeCoolDown = 1f;
+    bool placeIsRunning = false;
+    IEnumerator Place()
+    {
+        placeIsRunning = true;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-                Instantiate(ObjectToBePlaced, WorldGrid.Point2Index(hit.point), Quaternion.identity);
-                OnPlace.
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
 
-
-            }
-
-
+            Instantiate(ObjectToBePlaced, WorldGrid.Point2Index(hit.point), Quaternion.identity);
+            OnPlace.Invoke();
         }
 
-
-
+        yield return new WaitForSeconds(placeCoolDown);
+        placeIsRunning = false;
     }
+
+
 }
